70288b2bedc2b105f422d92ce524fe132f723949
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2022-02-14 13:27:05 -0600

    Added a new maven project to hold image processing code using imagej

---
+++ Image Processing/imagejprocessing/src/main/java/com/imagejprocessing/App.java
@@ -0,0 +1,13 @@
+package com.imagejprocessing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
